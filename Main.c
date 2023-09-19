fd7f925120b71aa75c5c7b3af4f4080f82209d1d
KushKansal <mrkushkansal@gmail.com>
2023-09-19 16:03:34 +0530

    Welcome Page Added BY KK

--- Main.c
+++ Main.c
@@ -4,3 +4,53 @@ int main()
   printf("Welcome to TaskScaces");
   return 0; 
 }
+void homePage()
+{       
+  printf("|⌈¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯⌉|  ");
+  printf("||   \\      /\\      / |===== |      |====== /=====\\ |\\    /| |=====    ||");
+  printf("||    \\    /  \\    /  |_____ |      |       |     | | \\  / | |_____    || ");
+  printf("||     \\  /    \\  /   |      |      |       |     | |  \\/  | |         || ");
+  printf("||      \\/      \\/    |===== |_____ |====== \\=====/ |      | |=====    || ");
+  printf("||                           TO TaskScapes                             ||   ");                                                   
+  printf("||     MENU:-                                                          ||   ");       
+  printf("||   1.                                                                ||   ");
+  printf("||   2.                                                                ||   ");
+  printf("||   3.CLOSE TASKSCAPES                                                ||   ");               
+  printf("|⌊_____________________________________________________________________⌋|   ");
+  printf("ENTER YOUR CHOICE::");
+  int choice;
+  scanf("%d",&choice);
+  switch(choice)
+  {
+      case 1:
+          break;
+      case 2:
+          break;
+      case 3:
+          exit();
+          break;
+      default:
+          exit();
+  }            
+}
+void exit()
+{ 
+  printf("Are you sure you want to exit?(YES/NO)");
+  char input[3];
+  scanf("%s", input);
+  if(strcmp(input,"YES")==0)
+  {
+      printf("|⌈¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯⌉|  ");
+      printf("|| ---------- |     |    /\\    |\\    | |  /      \\     / |=====| |    |   ||  ");
+      printf("||     ||     |     |   /  \\   | \\   | | /        \\   /  |     | |    |   ||  ");
+      printf("||     ||     |=====|  /====\\  |  \\  | |/          \\=/   |     | |    |   ||  ");
+      printf("||     ||     |     | /      \\ |   \\ | |\\           |    |     | |    |   ||  ");
+      printf("||     ||     |     |/        \\|    \\| | \\          |    |=====| |====|   ||  ");
+      printf("|⌊________________________________________________________________________⌋|  ");
+      exit(0);
+  }
+  else          
+  {
+      homePage();
+  }
+}
