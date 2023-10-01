6210fb2809d43055a11427bbe549911e13619e50
KushKansal <mrkushkansal@gmail.com>
2023-10-01 16:56:34 +0530

    Updated

--- Main.c
+++ Main.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
+#include<conio.h>
 #include<string.h>
 #include<math.h>
+struct Credentials
+{
+    long loginID;
+    long Password;
+    bool verified;
+};
 void escape();
+void login();
+void createAccount();
+void guest();
 void homePage();
+int mainMenu();
+void taskMenu();
 int main()
 {
   homePage();
@@ -18,11 +31,12 @@ void homePage()
   printf("||    \\    /  \\    /  |_____ |      |       |     | | \\  / | |_____    ||\n");
   printf("||     \\  /    \\  /   |      |      |       |     | |  \\/  | |         ||\n");
   printf("||      \\/      \\/    |===== |_____ |====== \\=====/ |      | |=====    ||\n");
-  printf("||                           TO TaskScapes                             ||\n");
+  printf("||                           TO TaskMaster X                           ||\n");
   printf("||     MENU:-                                                          ||\n");
-  printf("||   1.                                                                ||\n");
-  printf("||   2.                                                                ||\n");
-  printf("||   3.CLOSE TASKSCAPES                                                ||\n");
+  printf("||   1.Login                                                           ||\n");
+  printf("||   2.Create a new account                                            ||\n");
+  printf("||   3.Guest Mode                                                      ||\n");
+  printf("||   3.CLOSE TASKMASTER X                                              ||\n");
   printf("||_____________________________________________________________________||\n");
   printf("ENTER YOUR CHOICE::");
   scanf("%d",&choice);
@@ -33,6 +47,8 @@ void homePage()
       case 2:
           break;
       case 3:
+          break;
+      case 4:
           escape();
           break;
       default:
@@ -49,13 +65,31 @@ void escape()
   {
       printf("\n  ________________________________________________________________________");
       printf("\n||                                                                        ||");
-      printf("\n|| ---------- |     |    /\\    |\\    | |  /      \\     / |=====| |    |   ||  ");
-      printf("\n||     ||     |     |   /  \\   | \\   | | /        \\   /  |     | |    |   ||  ");
-      printf("\n||     ||     |=====|  /====\\  |  \\  | |/          \\=/   |     | |    |   ||  ");
-      printf("\n||     ||     |     | /      \\ |   \\ | |\\           |    |     | |    |   ||  ");
-      printf("\n||     ||     |     |/        \\|    \\| | \\          |    |=====| |====|   ||  ");
-      printf("\n||________________________________________________________________________||  ");
+      printf("\n|| ---------- |     |    /\\    |\\    | |  /      \\     / |=====| |    |   ||");
+      printf("\n||     ||     |     |   /  \\   | \\   | | /        \\   /  |     | |    |   ||");
+      printf("\n||     ||     |=====|  /====\\  |  \\  | |/          \\=/   |     | |    |   ||");
+      printf("\n||     ||     |     | /      \\ |   \\ | |\\           |    |     | |    |   ||");
+      printf("\n||     ||     |     |/        \\|    \\| | \\          |    |=====| |====|   ||");
+      printf("\n||________________________________________________________________________||\n");
       exit(0);
   }
   else  homePage();
 }
+int mainMenu()
+{
+    //This function is used to print the Menu of the Todo list and returns a choice entered by the user.
+    int choice;
+    system("cls");          //clears the screen.
+    printf("\n\nEnter your choice : ");
+    printf("\n\t\t\t\t\t\t 1. See your ToDo List. \n");
+    printf("\n\t\t\t\t\t\t 2. Update your ToDo List. \n");
+    printf("\n\t\t\t\t\t\t 3. Create a new ToDo List. \n");
+    printf("\n\t\t\t\t\t\t 4. Delete your ToDo List. \n");
+    printf("\n\t\t\t\t\t\t 5. Edit task(s) in your ToDo List. \n");
+    printf("\n\t\t\t\t\t\t 6. Exit ");
+    printf("\n\n");
+    printf("\n\t\t\t\t\t\t Enter your choice \n\t\t\t\t\t\t --> ");
+    scanf("%d",&choice);
+    return choice;
+}
+
