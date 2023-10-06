7b98a0844d679c984796f1a16ea14a7f2a4107e4
KushKansal <mrkushkansal@gmail.com>
2023-10-06 14:32:10 +0530

    Added Create an Account Feature

--- Main.c
+++ Main.c
@@ -4,6 +4,7 @@
 #include<conio.h>
 #include<string.h>
 #include<math.h>
+#include <unistd.h>
 
 struct Credentials
 {
@@ -19,6 +20,7 @@ void guest();
 void homePage();
 int mainMenu();
 void taskMenu();
+bool validate(int dob);
 
 int main()
 {
@@ -28,39 +30,42 @@ int main()
 
 void homePage()
 {
-  int choice;
-  printf("\n  _____________________________________________________________________\n");
-  printf("||                                                                     ||\n");
-  printf("||   \\      /\\      / |===== |      |====== /=====\\ |\\    /| |=====    ||\n");
-  printf("||    \\    /  \\    /  |_____ |      |       |     | | \\  / | |_____    ||\n");
-  printf("||     \\  /    \\  /   |      |      |       |     | |  \\/  | |         ||\n");
-  printf("||      \\/      \\/    |===== |_____ |====== \\=====/ |      | |=====    ||\n");
-  printf("||                           TO TaskMaster X                           ||\n");
-  printf("||     MENU:-                                                          ||\n");
-  printf("||   1.Login                                                           ||\n");
-  printf("||   2.Create a new account                                            ||\n");
-  printf("||   3.Guest Mode                                                      ||\n");
-  printf("||   3.CLOSE TASKMASTER X                                              ||\n");
-  printf("||_____________________________________________________________________||\n");
-  printf("ENTER YOUR CHOICE::");
-  scanf("%d",&choice);
-  switch(choice)
-  {
-      case 1:
-          login();
-          break;
-      case 2:
-          createAccount();
-          break;
-      case 3:
-          guest();
-          break;
-      case 4:
-          escape();
-          break;
-      default:
-          escape();
-  }
+    system("cls");
+    system("color 5F");
+    int choice;
+    printf("\n  _____________________________________________________________________\n");
+    printf("||                                                                     ||\n");
+    printf("||   \\      /\\      / |===== |      |====== /=====\\ |\\    /| |=====    ||\n");
+    printf("||    \\    /  \\    /  |_____ |      |       |     | | \\  / | |_____    ||\n");
+    printf("||     \\  /    \\  /   |      |      |       |     | |  \\/  | |         ||\n");
+    printf("||      \\/      \\/    |===== |_____ |====== \\=====/ |      | |=====    ||\n");
+    printf("||                           TO TaskMaster X                           ||\n");
+    printf("||     MENU:-                                                          ||\n");
+    printf("||   1.Login                                                           ||\n");
+    printf("||   2.Create a new account                                            ||\n");
+    printf("||   3.Guest Mode                                                      ||\n");
+    printf("||   3.CLOSE TASKMASTER X                                              ||\n");
+    printf("||_____________________________________________________________________||\n");
+    printf("ENTER YOUR CHOICE::");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+        case 1:
+            login();
+            break;
+        case 2:
+            createAccount();
+            break;
+        case 3:
+            guest();
+            break;
+        case 4:
+            escape();
+            break;
+        default:
+            escape();
+    }
 }
 
 void escape()
@@ -69,6 +74,7 @@ void escape()
   char ch; 
   scanf("%c", &ch);
   scanf("%c", &ch);
+
   if(ch=='Y'||ch=='y')
   {
       printf("\n  ________________________________________________________________________");
@@ -81,7 +87,10 @@ void escape()
       printf("\n||________________________________________________________________________||\n");
       exit(0);
   }
-  else  homePage();
+  else  
+  {
+    homePage();
+  }
 }
 
 int mainMenu()
@@ -102,10 +111,6 @@ int mainMenu()
     return choice;
 }
 
-void taskMenu()
-{
-}
-
 void login()
 {
     struct Credentials user;
@@ -122,6 +127,7 @@ void login()
         return;
     }
     user.verified=false;
+
     while(fread(&user, sizeof(struct Credentials),1,file)) 
     {
         if(user.loginID==user.loginID&&user.Password==user.Password) 
@@ -131,6 +137,7 @@ void login()
         }
     }
     fclose(file);
+
     if(user.verified) 
     {
         printf("Login successful!\n");
@@ -158,12 +165,66 @@ void login()
 
 void createAccount()
 {
+    char name[20];
+    int dob=0;
+    struct Credentials newUser;
+    printf("To Create a New Account\n");
+    printf("Enter your fUll name : ");
+    gets(name);
+    retry:
+    printf("Enter your date of birth(format DDMMYY) : ");
+    scanf("%d",&dob);
+
+    if(!validate(dob))
+    {
+        printf("Wrong Date of birth......Retry.....");
+        goto retry;
+    }
+
+    printf("Enter a new Login ID: ");
+    scanf("%ld",&newUser.loginID);    
+    // Check if the login ID already exists
+    printf("Loading.....");
+    sleep(2);
+    FILE *file=fopen("Credentials.txt","r");
+    if(file!=NULL) 
+    {
+        struct Credentials existingUser;
+        while(fread(&existingUser,sizeof(struct Credentials),1,file)) 
+        {
+            if(existingUser.loginID==newUser.loginID) 
+            {
+                fclose(file);
+                printf("Login ID already exists. Please choose a different one.\n");
+                return;
+            }
+        }
+        fclose(file);
+    }
+
+    printf("Enter a new Password: ");
+    scanf("%ld", &newUser.Password);
+    newUser.verified=true;
+    file=fopen("Credentials.txt","a");// Open the file in append mode
+    if(file==NULL)
+    {
+        printf("Error opening the file for writing.\n");
+        return;
+    }
+    
+    //Writing the new user's credentials to the file
+    fwrite(&newUser, sizeof(struct Credentials), 1, file);
+    fclose(file);
+    printf("Account created successfully!\n");
+    printf("Now you will be guided to login page.\nEnter your credentials there to login");
+    sleep(3);
 }
 
 void guest()
 {
     
     printf("\nGuest Mode Loading.....\n");
+    sleep(3);
     struct Credentials guestUser;
     guestUser.loginID = 12345;
     guestUser.Password = 12345;
@@ -172,4 +233,16 @@ void guest()
     printf("Welcome, Guest!\n");
     int choice = mainMenu();
     // Handle the choice based on user's input from main menu
+}
+
+bool validate(int dob)
+{
+    int day=dob/10000;
+    int month=(dob/100)%100;
+    int year=dob%100;
+    if(year<0||year>99||month<1||month>12||day<1||day>31)
+        return false;
+    if((month==2&&day>29)||((month==4||month==6||month==9||month==11)&&day>30))
+        return false;
+    return true;
 }
