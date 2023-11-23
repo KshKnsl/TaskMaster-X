0e2f155a79e8265ad67fa6acaf73a4abb4403355
KushKansal <mrkushkansal@gmail.com>
2023-11-23 11:23:10 +0530

    Syntax correction

--- taskmaster.c
+++ taskmaster.c
@@ -1312,7 +1312,7 @@ void filterToDoList(struct Credentials user)
             // Filter by Priority
             int priority;
             printf("Enter priority to filter tasks: ");
-            scanf("%d", &1priority);
+            scanf("%d", &priority);
 
             system("color 0E");  // Change text to yellow and background to black
 
